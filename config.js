50ca0edbc5dc3bb49cf16dc2410c3ac7971ce559
Idrissbop <161233860+Idrissbop@users.noreply.github.com>
2024-12-21 10:34:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "eiR1GYKY#uM3ItQ79m3HY0Hb5nGiPZkFv-2bR5qstWn18eb3nj8A", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
